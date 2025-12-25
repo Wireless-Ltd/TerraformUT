16299301f97fb1df18a119c79998fb9b9432ce59
Umair2231 <jafri223@gmail.com>
2025-12-25 18:32:55 +0000

    provider with backend

--- ParentModule_Data/provider.tf
+++ ParentModule_Data/provider.tf
@@ -5,6 +5,14 @@ terraform {
       version = "4.57.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "RG_BKEND_UT"             # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    storage_account_name = "stgutbkend"             # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "bkendcontainer"                # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "Terraform.tfstate" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+    
+    }
+
 }
 
 provider "azurerm" {
